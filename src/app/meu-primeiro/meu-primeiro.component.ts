be7278fdb7820bcbb9aec881068774bbb99aafc6
elaine <e.cristina00@gmail.com>
2021-09-27 10:57:46 -0300

    Primeiros passos com o angular

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu primeiro component com o Angular</p>
+  `
+})
+export class MeuPrimeiroComponent{ }
