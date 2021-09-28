34d584f7042d3b4bec0cf89557ea702d57851ea2
elaine <e.cristina00@gmail.com>
2021-09-28 01:09:22 -0300

    Criando o componente Curso Detalhe

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   exports:[
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ]
 })
 export class CursosModule { }
